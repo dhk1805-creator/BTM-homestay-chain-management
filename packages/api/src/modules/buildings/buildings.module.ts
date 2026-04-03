63865fda5484d478dd4f2d2c456baa3f8a6ef3b1
dhk1805-creator <dhk1805@gmail.com>
2026-04-03 16:30:54 +0700

    Update buildings.module.ts

--- packages/api/src/modules/buildings/buildings.module.ts
+++ packages/api/src/modules/buildings/buildings.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { BuildingsController } from './buildings.controller';
 import { BuildingsService } from './buildings.service';
-import { PrismaService } from '../../common/prisma.service';
 
 @Module({
   controllers: [BuildingsController],
-  providers: [BuildingsService, PrismaService],
+  providers: [BuildingsService],
   exports: [BuildingsService],
 })
 export class BuildingsModule {}
