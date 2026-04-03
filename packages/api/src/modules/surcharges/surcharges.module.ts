1effeab3ee2463f241bccdabced77ba21e133051
dhk1805-creator <dhk1805@gmail.com>
2026-04-03 16:33:11 +0700

    Update surcharges.module.ts

--- packages/api/src/modules/surcharges/surcharges.module.ts
+++ packages/api/src/modules/surcharges/surcharges.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SurchargesController } from './surcharges.controller';
 import { SurchargesService } from './surcharges.service';
-import { PrismaService } from '../../common/prisma.service';
 
 @Module({
   controllers: [SurchargesController],
-  providers: [SurchargesService, PrismaService],
+  providers: [SurchargesService],
   exports: [SurchargesService],
 })
 export class SurchargesModule {}
